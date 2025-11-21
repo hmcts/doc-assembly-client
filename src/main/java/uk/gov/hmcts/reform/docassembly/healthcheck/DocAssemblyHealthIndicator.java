e8a3d6bdbb6842dc37695ca26fffa7aa13b2f36c
michaeljacob <Michael.Jacob@HMCTS.NET>
2025-11-21 09:52:46 +0000

    fix compile errors

--- src/main/java/uk/gov/hmcts/reform/docassembly/healthcheck/DocAssemblyHealthIndicator.java
+++ src/main/java/uk/gov/hmcts/reform/docassembly/healthcheck/DocAssemblyHealthIndicator.java
@@ -2,8 +2,8 @@ package uk.gov.hmcts.reform.docassembly.healthcheck;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.actuate.health.Health;
-import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.health.contributor.Health;
+import org.springframework.boot.health.contributor.HealthIndicator;
 import uk.gov.hmcts.reform.docassembly.DocAssemblyApi;
 
 public class DocAssemblyHealthIndicator implements HealthIndicator {
